222e425d1cec70d0370e14e3751893739059d0db
Tim Columbia <tjc13@columbia.edu>
2021-04-15 18:55:19 -0400

    allow variable file name handles

--- timed_screencapture.sh
+++ timed_screencapture.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+# timed_screencapture.sh
 # Choose the number of captures and how many seconds between captures
 # TO DO: add switches to make a proper utility of this
 #        ...and Brett Terpstra's clever switch documentation trick
 
-LOOPS=4
-SLEEPSECS=5
+LOOPS=45
+SLEEPSECS=60
+HANDLE="ADV-20-KS-003"
 
 echo "Beginning timed_screencapture. $LOOPS iterations,  every $SLEEPSECS seconds."
 say "Beginning timed_screencapture. $LOOPS iterations,  every $SLEEPSECS seconds."
 
 for numb in $(seq 1 $LOOPS); do
-	DATETIME=$(date +"%Y-%m-%d-%H%M%S")
-	FNAME="screenshot_$DATETIME.png"
+	DATETIME=$(date +"%Y-%m-%d-%H.%M.%S")
+	FNAME="$HANDLE~$DATETIME.png"
 	echo "$FNAME"
 	screencapture -m $FNAME
 	sleep "$SLEEPSECS"
